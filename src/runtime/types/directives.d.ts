fa5d708fb6993ebe018fe4f818c110938b79b673
Carles Mitjans <carles.mitjans.coma@gmail.com>
2024-01-16 22:53:41 +0100

    docs: add JSDoc for posthog capture events

--- src/runtime/types/directives.d.ts
+++ src/runtime/types/directives.d.ts
@@ -2,7 +2,18 @@ import type { ObjectDirective } from 'vue';
 
 declare global {
   export interface PosthogCaptureEvent {
+    /**
+     * Event name
+     *
+     * @docs https://posthog.com/docs/product-analytics/capture-events
+     */
     name: string;
+
+    /**
+     * Event properties
+     *
+     * @docs https://posthog.com/docs/product-analytics/capture-events#setting-event-properties
+     */
     properties?: Record<string, any>;
   }
 }
