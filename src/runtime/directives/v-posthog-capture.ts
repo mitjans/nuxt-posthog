5af68446b4cc4957772b0836788c2dd031e1b372
Carles Mitjans <carles.mitjans.coma@gmail.com>
2024-12-09 13:26:35 +0100

    refactor: avoid nested ifs

--- src/runtime/directives/v-posthog-capture.ts
+++ src/runtime/directives/v-posthog-capture.ts
@@ -4,16 +4,16 @@ import type { ObjectDirective, FunctionDirective } from 'vue';
 const directive: FunctionDirective<HTMLElement, PostHogCaptureEvent | string> = (el, { value, arg }) => {
   const { $clientPosthog } = useNuxtApp();
 
-  if (!el.hasAttribute('posthog-listener')) {
-    el.setAttribute('posthog-listener', 'true');
+  if (el.hasAttribute('posthog-listener')) return;
 
-    el.addEventListener(arg ?? 'click', () => {
-      if (!$clientPosthog) return;
+  el.setAttribute('posthog-listener', 'true');
 
-      if (typeof value === 'string') $clientPosthog.capture(value);
-      else $clientPosthog.capture(value.name, value.properties);
-    });
-  }
+  el.addEventListener(arg ?? 'click', () => {
+    if (!$clientPosthog) return;
+
+    if (typeof value === 'string') $clientPosthog.capture(value);
+    else $clientPosthog.capture(value.name, value.properties);
+  });
 };
 
 export const vPostHogCapture: ObjectDirective = {
