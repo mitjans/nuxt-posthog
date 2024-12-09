de0efe96a3e7f8e6fd6bdae1018817f0ee4a66b9
Carles Mitjans <carles.mitjans.coma@gmail.com>
2024-12-09 13:24:40 +0100

    fix: avoid duplicating events

--- src/runtime/directives/v-posthog-capture.ts
+++ src/runtime/directives/v-posthog-capture.ts
@@ -4,12 +4,16 @@ import type { ObjectDirective, FunctionDirective } from 'vue';
 const directive: FunctionDirective<HTMLElement, PostHogCaptureEvent | string> = (el, { value, arg }) => {
   const { $clientPosthog } = useNuxtApp();
 
-  el.addEventListener(arg ?? 'click', () => {
-    if (!$clientPosthog) return;
+  if (!el.hasAttribute('posthog-listener')) {
+    el.setAttribute('posthog-listener', 'true');
 
-    if (typeof value === 'string') $clientPosthog.capture(value);
-    else $clientPosthog.capture(value.name, value.properties);
-  });
+    el.addEventListener(arg ?? 'click', () => {
+      if (!$clientPosthog) return;
+
+      if (typeof value === 'string') $clientPosthog.capture(value);
+      else $clientPosthog.capture(value.name, value.properties);
+    });
+  }
 };
 
 export const vPostHogCapture: ObjectDirective = {
